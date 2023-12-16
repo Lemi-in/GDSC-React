c356785298f0c0f670ea390e84e01c48be7b8634
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2023-12-16 13:51:56 +0300

    Create  TodoForm.js

---
+++ week5/src/components/TodoForm.js
@@ -0,0 +1,27 @@
+
+import React, { useState } from 'react';
+
+const TodoForm = ({ onAdd }) => {
+  const [text, setText] = useState('');
+
+  const handleAdd = () => {
+    if (text.trim() !== '') {
+      onAdd(text);
+      setText('');
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
+
+export default TodoForm;
