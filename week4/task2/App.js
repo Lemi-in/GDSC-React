aecdfc3385ea039c456b8449290e799b891eb319
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2023-12-16 13:10:23 +0300

    Create App.js

---
+++ week4/task2/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Group10Members from './mapping'; 
+
+const App = () => {
+  return (
+    <div>
+      <h1>GDSC</h1>
+      <Group10Members />
+    </div>
+  );
+};
+
+export default App;
