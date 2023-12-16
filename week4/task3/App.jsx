e2beab3b3eb006f567f79c26b9a98cb41adc451c
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2023-12-16 13:24:28 +0300

    Create App.jsx

---
+++ week4/task3/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Box.css';
+
+const App = () => {
+  return (
+    <div className="red-box">
+      <h1>GDSC Red Box</h1>
+    </div>
+  );
+};
+
+export default App;
+
+  
