fdb9bda37de5ff0c1533c02137d4ff116376624b
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2023-12-22 14:05:58 +0300

    Create App.jsx

---
+++ month-2/week2/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import ShoppingCart from './ShoppingCart';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <ShoppingCart />
+    </div>
+  );
+}
+
+export default App;
