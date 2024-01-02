5b445a94ff88755d3da177fe76e6e819bc232fc9
Lemesa Elias <149191865+Lemi-in@users.noreply.github.com>
2024-01-02 20:43:26 +0300

    Update NavBar.jsx

--- month-2/week3/src/components/NavBar.jsx
+++ month-2/week3/src/components/NavBar.jsx
@@ -1,1 +1,18 @@
 
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <NavLink to="/" activeClassName="active" exact>
+        Home
+      </NavLink>
+      <NavLink to="/cart" activeClassName="active">
+        Cart
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
